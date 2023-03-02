ae4b8370dc483896c623b1d4e422d0cb25317df5
Connor Elder <connorelder1@gmail.com>
2023-03-01 20:50:57 -0700

    Add project files.

---
+++ Mission9_celder20/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Mission9_celder20.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9_celder20.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes ="page-blah")]
+    public class PaginationTagHelper : TagHelper
+    {
+        //Dynamically create the page links for us
+        private IUrlHelperFactory uhf;
+
+        public PaginationTagHelper (IUrlHelperFactory temp)
+        {
+            uhf = temp;
+        }
+        [ViewContext]
+        [HtmlAttributeNotBound]
+
+        public ViewContext vc { get; set; }
+
+        //Different than the View context
+        public PageInfo PageBlah { get; set; }
+        public string PageAction { get; set; }
+
+        public override void Process (TagHelperContext thc, TagHelperOutput tho)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i <= PageBlah.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+
+                tb.Attributes["href"] = uh.Action(PageAction, new{ pageNum = i });
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+                final.InnerHtml.AppendHtml(" ");
+            }
+
+            tho.Content.AppendHtml(final.InnerHtml);
+        }
+    }
+}
